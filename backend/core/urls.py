9c586c8370f141bdc66ee2cfae3fa70f6fb6f8f0
dokwako <denzilwawire418@gmail.com>
2025-03-08 23:05:39 +0300

    new

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,6 +3,7 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from .views import MerchantViewSet, DeliveryPaternViewSet, OrderViewSet
+from django.http import JsonResponse
 
 
 # Create a router and register our viewset with it.
@@ -12,6 +13,10 @@ router.register(r'merchants', MerchantViewSet)  # merchants api
 router.register(r'deliverypaterns', DeliveryPaternViewSet) # deliverypaterns api
 router.register(r'orders', OrderViewSet) # orders api
 
+
+def home_view(request):
+    return JsonResponse({'message':'Welcome to direct conect delivery API'})
 urlpatterns = [
+    path('',home_view, name = 'home'), # root url
     path('api/', include(router.urls)),
 ]
